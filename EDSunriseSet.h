1a375a3ce4c03b19644a5c82cdd555828056dbaa
Marc Brown <marc@creativemess.com>
2013-06-25 14:49:31 -0400

    Added ARC pre-processor check

--- EDSunriseSet.h
+++ EDSunriseSet.h
@@ -19,6 +19,10 @@
 
 #import <Foundation/Foundation.h>
 
+#if ! __has_feature(objc_arc)
+#error This file must be compiled with ARC. Either turn on ARC for the project or use -fobjc-arc flag in this file.
+#endif
+
 @interface EDSunriseSet : NSObject
 {
 @private
